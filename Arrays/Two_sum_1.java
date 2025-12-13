53d42f98672917bf580a2f7e4995514b85527603
Komal <panwarkomal2003@gmail.com>
2025-12-13 23:45:22 +0530

    n

---
+++ Arrays/Two_sum_1.java
@@ -0,0 +1,28 @@
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         for(int i=0;i<nums.length-1;i++){
+//             for(int j=i+1;j<nums.length;j++){
+//                 if(nums[i]+nums[j]==target){
+//                     return new int[]{i,j};
+//                 }
+//             }
+//         }
+//         return new int[0];
+//     }
+// }
+
+class Solution{
+    public int[] twoSum(int[] nums, int target){
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for(int i =0;i<nums.length;i++){
+            int remain= target-nums[i];
+            if(map.containsKey(remain))
+                return new int[]{map.get(remain),i};
+            
+            map.put(nums[i],i);
+        }
+
+        return new int[0];
+    }
+}
