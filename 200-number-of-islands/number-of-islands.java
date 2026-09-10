dcade69b514218516522ffb9076f255d4bb406c4
Komal Panwar <panwarkomal2003@gmail.com>
2026-09-11 02:09:50 +0530

    Time: 3 ms (87.64%) | Memory: 52.1 MB (71.67%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,58 +1,69 @@
 class Solution {
-    class Pair {
-        int first;
-        int second;
-        public Pair(int first, int second) {
-            this.first = first;
-            this.second = second;
-        }
-    }
+    final int diff[][]={{0,-1},{0,1},{-1,0},{1,0}};
+    // class Pair {
+    //     int first;
+    //     int second;
+    //     public Pair(int first, int second) {
+    //         this.first = first;
+    //         this.second = second;
+    //     }
+    // }
 
-    private void bfs(int row, int col, char[][] grid, int[][] visited) {
-        visited[row][col] = 1;
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(row, col));
-        int n = grid.length;
-        int m = grid[0].length;
+    // private void bfs(int row, int col, char[][] grid, int[][] visited) {
+    //     visited[row][col] = 1;
+    //     Queue<Pair> q = new LinkedList<>();
+    //     q.add(new Pair(row, col));
+    //     int n = grid.length;
+    //     int m = grid[0].length;
 
-        // Direction arrays for 4-directional traversal: Up, Right, Down, Left
-        int[] dRow = {-1, 0, 1, 0};
-        int[] dCol = {0, 1, 0, -1};
+    //     // Direction arrays for 4-directional traversal: Up, Right, Down, Left
+    //     int[] dRow = {-1, 0, 1, 0};
+    //     int[] dCol = {0, 1, 0, -1};
 
-        while (!q.isEmpty()) {
-            Pair current = q.poll();
-            int r = current.first;
-            int c = current.second;
+    //     while (!q.isEmpty()) {
+    //         Pair current = q.poll();
+    //         int r = current.first;
+    //         int c = current.second;
 
-            for (int i = 0; i < 4; i++) {
-                int nrow = r + dRow[i];
-                int ncol = c + dCol[i];
+    //         for (int i = 0; i < 4; i++) {
+    //             int nrow = r + dRow[i];
+    //             int ncol = c + dCol[i];
 
-                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m 
-                        && visited[nrow][ncol] != 1 && grid[nrow][ncol] == '1') {
-                    visited[nrow][ncol] = 1;
-                    q.add(new Pair(nrow, ncol));
-                }
-            }
-        }
-    }
+    //             if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m 
+    //                     && visited[nrow][ncol] != 1 && grid[nrow][ncol] == '1') {
+    //                 visited[nrow][ncol] = 1;
+    //                 q.add(new Pair(nrow, ncol));
+    //             }
+    //         }
+    //     }
+    // }
 
     public int numIslands(char[][] grid) {
         if (grid == null || grid.length == 0) return 0;
 
         int n = grid.length;
         int m = grid[0].length;
-        int[][] visited = new int[n][m];
         int count = 0;
 
         for (int row = 0; row < n; row++) {
             for (int col = 0; col < m; col++) {
-                if (visited[row][col] != 1 && grid[row][col] == '1') {
+                if (grid[row][col] == '1') {
                     count++;
-                    bfs(row, col, grid, visited);
+                    dfs(grid, n, m, row, col);
                 }
             }
         }
         return count;
+    }
+
+    
+    private void dfs(char[][] grid, int R, int C, int row, int col){
+        grid[row][col]='0';
+        for(int i=0; i<4; i++){
+            int ar=row+diff[i][0], ac=col+diff[i][1];
+            if(ar>=0 && ar<R && ac>=0 && ac<C && grid[ar][ac]=='1'){
+                dfs(grid, R, C, ar, ac);
+            }
+        }
     }
 }
