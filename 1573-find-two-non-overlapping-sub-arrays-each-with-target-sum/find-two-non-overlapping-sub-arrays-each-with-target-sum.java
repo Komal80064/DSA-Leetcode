a7d3bf971797d9f245dd0f794ca7b66c9c8d3408
Komal Panwar <panwarkomal2003@gmail.com>
2026-09-17 20:54:32 +0530

    Time: 6 ms (99.10%) | Memory: 91.1 MB (50.67%) - LeetSync

--- 1573-find-two-non-overlapping-sub-arrays-each-with-target-sum/find-two-non-overlapping-sub-arrays-each-with-target-sum.java
+++ 1573-find-two-non-overlapping-sub-arrays-each-with-target-sum/find-two-non-overlapping-sub-arrays-each-with-target-sum.java
@@ -1,25 +1,31 @@
 class Solution {
-    public int minSumOfLengths(int[] A, int k) {
-        int n = A.length;
-        int res = n + 1, sum = 0, i = 0;
-
-        int[] dp = new int[n + 1];
-        Arrays.fill(dp, n);
-
-        for (int j = 0; j < n; j++) {
-            sum += A[j];
-
-            while (sum > k)
-                sum -= A[i++];
-
-            dp[j + 1] = dp[j];
-
-            if (sum == k) {
-                res = Math.min(res, j - i + 1 + dp[i]);
-                dp[j + 1] = Math.min(dp[j], j - i + 1);
+    public int minSumOfLengths(int[] arr, int target) {
+        int n = arr.length;
+        int[] minLen = new int[n];
+        int l = 0, sum = 0;
+        int ans = Integer.MAX_VALUE;
+        int best = Integer.MAX_VALUE;
+        
+        for (int r = 0; r < n; r++) {
+            sum += arr[r];
+            
+            while (sum > target) {
+                sum -= arr[l++];
             }
+            
+            if (sum == target) {
+                int currLen = r - l + 1;
+                
+                if (l > 0 && minLen[l - 1] != Integer.MAX_VALUE) {
+                    ans = Math.min(ans, currLen + minLen[l - 1]);
+                }
+                
+                best = Math.min(best, currLen);
+            }
+            
+            minLen[r] = best;
         }
-
-        return res == n + 1 ? -1 : res;
+        
+        return ans == Integer.MAX_VALUE ? -1 : ans;
     }
 }
