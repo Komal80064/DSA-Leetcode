175448926f39766a60fbd1b9277018f14ed25bed
Komal Panwar <panwarkomal2003@gmail.com>
2026-09-26 02:42:52 +0530

    Time: 9 ms (82.68%) | Memory: 47.7 MB (21.26%) - LeetSync

--- Daily/1188-brace-expansion-ii/brace-expansion-ii.java
+++ Daily/1188-brace-expansion-ii/brace-expansion-ii.java
@@ -1,31 +1,60 @@
 class Solution {
-    TreeSet<String> ans = new TreeSet<>();
-
-    void dfs(String s) {
-        int r = s.indexOf('}');
-
-        // No braces left
-        if (r == -1) {
-            ans.add(s);
-            return;
-        }
-
-        // Find matching '{'
-        int l = s.lastIndexOf('{', r);
-
-        String left = s.substring(0, l);
-        String right = s.substring(r + 1);
-
-        // Content inside { }
-        String inside = s.substring(l + 1, r);
-
-        for (String part : inside.split(",")) {
-            dfs(left + part + right);
-        }
+    public List<String> braceExpansionII(String expression) {
+        Set<String> set = parse(expression, new int[]{0});
+        List<String> ans = new ArrayList<>(set);
+        Collections.sort(ans);
+        return ans;
     }
 
-    public List<String> braceExpansionII(String expression) {
-        dfs(expression);
-        return new ArrayList<>(ans);
+    private Set<String> parse(String s, int[] i) {
+        Set<String> res = new HashSet<>();
+        res.add("");
+
+        while (i[0] < s.length() && s.charAt(i[0]) != '}') {
+            Set<String> cur;
+
+            if (s.charAt(i[0]) == '{') {
+                i[0]++;
+                cur = parse(s, i);
+                i[0]++;
+            } else {
+                cur = new HashSet<>();
+                cur.add(String.valueOf(s.charAt(i[0]++)));
+            }
+
+            Set<String> next = new HashSet<>();
+
+            for (String a : res) {
+                for (String b : cur) {
+                    next.add(a + b);
+                }
+            }
+
+            res = next;
+
+            if (i[0] < s.length() && s.charAt(i[0]) == ',') {
+                i[0]++;
+                res.addAll(parseUnion(s, i));
+                break;
+            }
+        }
+
+        return res;
+    }
+
+    private Set<String> parseUnion(String s, int[] i) {
+        Set<String> res = new HashSet<>();
+
+        while (i[0] < s.length() && s.charAt(i[0]) != '}') {
+            res.addAll(parse(s, i));
+
+            if (i[0] < s.length() && s.charAt(i[0]) == ',') {
+                i[0]++;
+            } else {
+                break;
+            }
+        }
+
+        return res;
     }
 }
